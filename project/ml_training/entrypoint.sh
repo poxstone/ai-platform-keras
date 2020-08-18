952e18ea5d7e09bc21cdda68672ee47bd50ffb00
poxstone <poxstone@gmail.com>
2020-08-18 10:54:24 -0500

    add git push tag to training

--- project/ml_training/entrypoint.sh
+++ project/ml_training/entrypoint.sh
@@ -1,6 +1,9 @@
 # Execute Training
 python -m trainer.task  --job-version ${MODEL_VERSION} --trainded-dir ${JOB_DIR} && \
+# git add tag to trigger
 git clone "${GIT_REPO}" "code_clone";
 cd "code_clone";
-git tag -a "${GIT_TAG}" -m "trained";
-git push origin "${GIT_TAG}";
+git tag -d "${GIT_TAGNAME}";
+git push -d origin "${GIT_TAGNAME}";
+git tag -a "${GIT_TAGNAME}" "${GIT_COMMIT}" -m "";
+git push origin "${GIT_TAGNAME}";
