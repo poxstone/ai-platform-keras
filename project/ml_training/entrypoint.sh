8023688883f3c6c0cda5dd656bf65ee1d894aa79
poxstone <poxstone@gmail.com>
2020-08-18 12:56:16 -0500

    update image to training

--- project/ml_training/entrypoint.sh
+++ project/ml_training/entrypoint.sh
@@ -7,3 +7,12 @@ git tag -d "${GIT_TAGNAME}";
 git push -d origin "${GIT_TAGNAME}";
 git tag -a "${GIT_TAGNAME}" "${GIT_COMMIT}" -m "";
 git push origin "${GIT_TAGNAME}";
+
+# publish image
+gcloud ai-platform versions create "${MODEL_NAME}${MODEL_VERSION}" --project "${GOOGLE_CLOUD_PROJECT}" \
+  --model "${MODEL_NAME}" \
+  --framework "tensorflow" \
+  --origin "${JOB_DIR}/${MODEL_NAME}/${MODEL_VERSION}/" \
+  --runtime-version "2.1" \
+  --python-version "3.7";
+  --quiet
